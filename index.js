971da692c983d799ded748f292cda4b3aa4a9bbd
Raihan Sharif <98370271+Raihan-10@users.noreply.github.com>
2025-01-13 23:27:15 +0600

    Add Files

---
+++ index.js
@@ -0,0 +1,20 @@
+const body = document.getElementById("main");
+const text=document.getElementsByTagName("button");
+
+
+
+
+function setColor(name){
+    body.style.background=name;
+}
+
+
+function randomColor(){
+    let red=Math.round(Math.random()*255);
+    let green=Math.round(Math.random()*255);
+    let blue=Math.round(Math.random()*255);
+
+    const color=`rgb(${red},${green}, ${blue})`;
+    body.style.backgroundColor=color;
+
+}
